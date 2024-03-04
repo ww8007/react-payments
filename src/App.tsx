3227e1a3dd58546a735021d30ad221b07ebccfdb
장동현 <54137044+ww8007@users.noreply.github.com>
2024-03-05 02:17:35 +0900

    Revert "feat: Router 추가 및 CardFunnel 렌더링 추가"
    This reverts commit 1c939ded4ccf7d5dde3b22068424a9690473f222.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import navigationPath from "./common/navigation/navigationPath";
-import CardFunnel from "./pages/CardFunnel";
-
 function App() {
-	return (
-		<div id='app'>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path={navigationPath.ADD_CARD_FUNNEL["CARD_LIST"]}
-						element={<CardFunnel />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+	return <>Payments</>;
 }
 
 export default App;
